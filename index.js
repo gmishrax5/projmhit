466e499e856e08154b2425ff39f17fc973ab3538
Your Name <user@example.com>
2025-12-17 00:27:48 +0530

    add this till 111 and strting project struct

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("heelo");
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: "./.env",
+
+});
+
+let myusername = process.env.database;
+
+console.log("value:", myusername);
+
+
+// dotenv.config({ path: '/custom/path/to/.env'})
+
+console.log("aaee haaalo ");
